06270f515357675aeee9d488bd3f659b64a34d5f
VinhHuuTran <tranhuuvinh1109@gmail.com>
2024-01-06 17:38:24 +0700

    hover

--- src/pages/chatSection/components/Message.tsx
+++ src/pages/chatSection/components/Message.tsx
@@ -1,5 +1,5 @@
 import { Message } from '@/api/chatAPI/chatAPI.types'
-import { Avatar, Container, Flex } from '@radix-ui/themes'
+import { Avatar, Box, Container, Flex, Heading, HoverCard, Text } from '@radix-ui/themes'
 import { DotPulse } from '@uiball/loaders'
 import styled from '@emotion/styled'
 import { useTheme } from '@emotion/react'
@@ -19,24 +19,50 @@ const MessageItem = ({ answer, question }: MessageProps) => {
   }
 
   return (
-    <Container size={'3'} py={'2'}>
-      <Flex direction={'column'} gap={'4'}>
-        <Flex gap={'2'} pl={'8'}>
-          <UserMsg>{question}</UserMsg>
-          {avatar ? (
-            <Avatar fallback={username.charAt(0)} src={avatar} alt={username} radius='full' />
-          ) : (
-            <Avatar fallback={username.charAt(0)} variant={'soft'} />
-          )}
+    <HoverCard.Root>
+      <HoverCard.Trigger>
+        <Container size={'3'} py={'2'} className='hover:cursor-default'>
+          <Flex direction={'column'} gap={'4'}>
+            <Flex gap={'2'} pl={'8'}>
+              <UserMsg>{question}</UserMsg>
+              {avatar ? (
+                <Avatar fallback={username.charAt(0)} src={avatar} alt={username} radius='full' />
+              ) : (
+                <Avatar fallback={username.charAt(0)} variant={'soft'} />
+              )}
+            </Flex>
+            <Flex gap={'2'} pr={'8'}>
+              <Avatar fallback={''} radius='full' src='http://localhost:3000/bot.jpg' />
+              <ChatBotMsg>
+                {answer ? answer : <DotPulse size={20} speed={1.3} color={colors.slate1} />}
+              </ChatBotMsg>
+            </Flex>
+          </Flex>
+        </Container>
+      </HoverCard.Trigger>
+      <HoverCard.Content>
+        <Flex gap='4'>
+          <Avatar
+            size='3'
+            fallback='R'
+            radius='full'
+            src='https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png'
+          />
+          <Box>
+            <Heading size='3' as='h3'>
+              Radix
+            </Heading>
+            <Text as='div' size='2' color='gray'>
+              @radix_ui
+            </Text>
+
+            <Text as='div' size='2' style={{ maxWidth: 300 }} mt='3'>
+              React components, icons, and colors for building high-quality, accessible UI.
+            </Text>
+          </Box>
         </Flex>
-        <Flex gap={'2'} pr={'8'}>
-          <Avatar fallback={''} radius='full' src='http://localhost:3000/bot.jpg' />
-          <ChatBotMsg>
-            {answer ? answer : <DotPulse size={20} speed={1.3} color={colors.slate1} />}
-          </ChatBotMsg>
-        </Flex>
-      </Flex>
-    </Container>
+      </HoverCard.Content>
+    </HoverCard.Root>
   )
 }
 
